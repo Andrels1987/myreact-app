7fa490f110b7146b32c5732b37af97b8f725483c
Andre Luis Silva <alspersonalmail@gmail.com>
2020-12-26 22:13:00 -0300

    Login page3

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,4 @@
-import React, { useEffect, useState } from 'react'
-import img1 from "./assets/img1.jpg"
-import img2 from "./assets/img2.jpg"
-import img3 from "./assets/img3.jpg"
+import React from 'react'
 
 const Login = ({
     login,
